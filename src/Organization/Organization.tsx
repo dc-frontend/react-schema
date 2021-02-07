f393520518a58751ce2940d319af2d42f59ecfcb
davidcraig <davidcraig1986@gmail.com>
2021-02-07 21:14:46 +0000

    Make url optional passable property.

--- src/Organization/Organization.tsx
+++ src/Organization/Organization.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 interface OrgProps {
   name: string,
+  url?: string,
   description?: string
 }
 
@@ -12,11 +13,11 @@ export default function Organization(props: OrgProps) {
   let schema = {
     "@context": "https://schema.org",
     "@type": "Organization",
-    "url": "https://notsafeforazeroth.netlify.app/",
     "name": props.name
   }
 
-  if (props.description) { schema["description"] = props.description }
+  if (props.url) { schema['url'] = props.url }
+  if (props.description) { schema['description'] = props.description }
 
   return <script
     type='application/ld+json'
