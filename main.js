3c31d1233fd3dc1938852f716f3799e1acf85cb7
davidcraig <davidcraig1986@gmail.com>
2021-02-07 21:15:37 +0000

    Add main.js.

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Organization", {
+  enumerable: true,
+  get: function get() {
+    return _Organization.Organization;
+  }
+});
+
+var _Organization = require("./dist/Organization");
