7f4bc7a2ee208f15c6f146b54b22ed573d5a3aa3
davidcraig <davidcraig1986@gmail.com>
2021-02-11 23:02:58 +0000

    Create occupation type.

---
+++ src/Occupation/Occupation.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { OptionalThingProps, ThingProps } from '../Thing/Thing'
+import Context from '../Context'
+
+const optionalProps = [
+  ...OptionalThingProps
+]
+
+export interface OccupationProps extends ThingProps {
+}
+
+/**
+ * Person Json
+ * @param props props for Person.
+ */
+export function OccupationJson(props: OccupationProps) {
+  let schema = { 'name': props.name, '@type': 'Occupation' }
+
+  optionalProps.forEach(p => {
+    if (props[p]) { schema[p] = props[p] }
+  })
+
+  return schema
+}
+
+/**
+ * @param {OrgProps} props
+ */
+export function Occupation(props: OccupationProps) {
+  const schema = Context(OccupationJson(props))
+
+  return <script
+    type='application/ld+json'
+    dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+  />
+}
+
+export default Occupation
