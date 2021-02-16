a92e8cba90a3af38794182378e6a5d599e85598b
davidcraig <davidcraig1986@gmail.com>
2021-02-16 08:39:28 +0000

    Add blog type.

---
+++ src/CreativeWork/Blog/Blog.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { PersonProps } from '../../Person/Person'
+import { CreativeWorkOptionalProps, CreativeWorkProps } from '../CreativeWork'
+import Context from '../../Context'
+
+const optionalProps = [
+  'blogPost',
+  'issn',
+  ...CreativeWorkOptionalProps
+]
+
+export interface BlogProps extends CreativeWorkProps {
+  blogPost?: object,
+  issn?: string
+}
+
+/**
+ * Person Json
+ * @param props props for Person.
+ */
+export function BlogJson(props: BlogProps) {
+  let schema = { 'name': props.name, '@type': 'Blog' }
+
+  optionalProps.forEach(p => {
+    if (props[p]) { schema[p] = props[p] }
+  })
+
+  return schema
+}
+
+/**
+ * @param {OrgProps} props
+ */
+export function Blog(props: BlogProps) {
+  const schema = Context(BlogJson(props))
+
+  return <script
+    type='application/ld+json'
+    dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+  />
+}
+
+export default Blog
